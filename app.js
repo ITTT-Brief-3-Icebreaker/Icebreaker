3965b8cb4ead0ead35f869cc8a830ad78711b531
emiliafrisell <emiliafrisell@gmail.com>
2020-11-17 13:32:41 +0100

    Trying out getting and displaying random data from the APIs

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-console.log("hej");
+let facts = [];
+let jokes = [];
 
-componentDidMount()
+function newData() {
+  let ID = Math.floor(Math.random() * 50);
+  document.querySelector('#line').innerHTML = 'titel: ' + facts[ID].title + '<br />body: ' + facts[ID].body;
+};
+
+function nextJoke() {
+  // console.log('button pressed');
+  // console.log(jokes)
+  let ID = Math.floor(Math.random() * 10);
+  document.querySelector('#quote').innerHTML = jokes.jokes[ID].joke ;
+};
+
+
+   function componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => facts = data)    
+    }
+
+    function getJokes() {
+      fetch('https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist&type=single&amount=10')
+        .then(response => response.json())
+        .then(data => jokes = data)    
+    }
+    
+
+
+// ALTERNATIVE WAY OF WRITING
 // async function componentDidMount() {
 //     console.log("fetch init")
 //     const response = await fetch('https://callook.info/W1AW/json')
@@ -9,13 +37,3 @@ componentDidMount()
 //     // const posts = [...this.state.posts, ...data]
 //     console.log("fetch done")
 //   }
-
-   function componentDidMount() {
-      console.log("fetch init")
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          console.log("fetch done")
-        })
-    }
---
+++ event-listeners.js
@@ -0,0 +1,7 @@
+// JOKES
+document.querySelector('#get_jokes').addEventListener('click', getJokes);
+document.querySelector('#next_joke').addEventListener('click', nextJoke) 
+
+// RANDOM LATIN
+document.querySelector('#get_lines').addEventListener('click', componentDidMount);
+document.querySelector('#next_line').addEventListener('click', newData) 
