77319508ca8cbb8750fbdc18cf43987f4c7326bd
Fanny Buhlér <fanny.buhler1@gmail.com>
2020-11-18 15:00:59 +0100

    Carousel with images of the cards

--- app.js
+++ app.js
@@ -38,3 +38,10 @@ function nextJoke() {
 //     // const posts = [...this.state.posts, ...data]
 //     console.log("fetch done")
 //   }
+
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
--- event-listeners.js
+++ event-listeners.js
@@ -7,5 +7,5 @@ document.querySelector('#get_lines').addEventListener('click', componentDidMount
 document.querySelector('#next_line').addEventListener('click', newData) 
 
 
-document.querySelector('#get_advice').addEventListener('click', getAdvice);
-document.querySelector('#next_advice').addEventListener('click', nextAdvice) 
+// document.querySelector('#get_advice').addEventListener('click', getAdvice);
+// document.querySelector('#next_advice').addEventListener('click', nextAdvice) 
