d582a89a559d46dfbd3af17c268e1dabca812c20
rafenasir <rafenasir@gmail.com>
2020-11-18 17:24:42 +0100

    added Basic Bootstrap to defin grids

---
+++ eventListner.js
@@ -0,0 +1,11 @@
+// JOKES
+document.querySelector('#get_jokes').addEventListener('click', getJokes);
+document.querySelector('#next_joke').addEventListener('click', nextJoke)
+
+// RANDOM LATIN
+document.querySelector('#get_lines').addEventListener('click', componentDidMount);
+document.querySelector('#next_line').addEventListener('click', newData)
+
+
+document.querySelector('#get_advice').addEventListener('click', getAdvice);
+document.querySelector('#next_advice').addEventListener('click', nextAdvice)
