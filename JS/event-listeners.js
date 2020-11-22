ceb982ebff3edb2c8541137c0cf4b1269d519a43
Emilia Frisell <emiliafrisell@gmail.com>
2020-11-22 17:06:55 +0100

    added listener for next fact and facts-mobile

--- JS/event-listeners.js
+++ JS/event-listeners.js
@@ -1,4 +1,5 @@
 let favourites = document.querySelector('.heart');
+// let facts = document.querySelector('#facts-mobile');
 
 // JOKES
 document.querySelector('#get_jokes').addEventListener('click', getJokes);
@@ -12,7 +13,15 @@ if (document.querySelector('#next_joke')) {
     document.querySelector('#next_joke').addEventListener('click', nextJoke) 
 }
 
+if (document.querySelector('#next-fact')) {
+    document.querySelector('#next-fact').addEventListener('click', nextFact) 
+}
 
 if (favourites) {
-    favourites.addEventListener('click', fillHeart)
+    favourites.addEventListener('click', fillHeart);
+}
+
+if (document.querySelector('#facts-mobile')) {
+    document.querySelector('#facts-mobile').addEventListener('click', getFacts);
+    console.log('gactalkdg')
 }
