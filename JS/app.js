8d9b9ab0735b58a5a480cafa9bac70ef416f4e33
Emilia Frisell <emiliafrisell@gmail.com>
2020-11-23 12:20:19 +0100

    removed unused code

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,6 @@
 let facts = [];
 let advice = [];
 
-function newData() {
-  let ID = Math.floor(Math.random() * 50);
-  document.querySelector('#line').innerHTML = 'titel: ' + facts[ID].title + '<br />body: ' + facts[ID].body;
-};
-
-// function nextJoke() {
-//   let ID = Math.floor(Math.random() * 10);
-//   document.querySelector('#quote').innerHTML = jokes.jokes[ID].joke;
-// };
-
-
-
-  // function componentDidMount() {
-  //     fetch('https://jsonplaceholder.typicode.com/posts')
-  //       .then(response => response.json())
-  //       .then(data => facts = data)    
-  //   }
 
   function getJokes() {
     console.log('get new Jokes')
@@ -35,18 +18,6 @@ function newData() {
           .then(data => {facts = data, localStorage.setItem('Facts', JSON.stringify(facts))})    
           // .then(jokes => )
       }
-    
-
-
-// ALTERNATIVE WAY OF WRITING
-// async function componentDidMount() {
-//     console.log("fetch init")
-//     const response = await fetch('https://callook.info/W1AW/json')
-//     const data = await response.json()
-//     console.log(data)
-//     // const posts = [...this.state.posts, ...data]
-//     console.log("fetch done")
-//   }
 
 var swiper = new Swiper('.swiper-container', {
   navigation: {
