09216135531adbb9038973a4632f3c95a9404498
Emilia Frisell <emiliafrisell@gmail.com>
2020-11-25 10:26:24 +0100

    added removed eventListener

--- JS/event-listeners.js
+++ JS/event-listeners.js
@@ -1,8 +1,9 @@
 
 
 // JOKES
+if (document.querySelector('#get_jokes')) {
 document.querySelector('#get_jokes').addEventListener('click', getJokes);
-
+}
 
 if (document.querySelector('#get_jokes_mobile')){
     document.querySelector('#get_jokes_mobile').addEventListener('click', getJokes);
@@ -20,6 +21,10 @@ if (document.querySelector('#next')) {
 
 if (document.querySelector('#facts-mobile')) {
     document.querySelector('#facts-mobile').addEventListener('click', getFacts);
-    console.log('gactalkdg')
 }
 
+if (document.querySelector('.remove')) {
+    document.querySelectorAll('.remove').forEach(element => {
+        console.log(element), element.addEventListener('click', removeFromAllFavourites)
+    })
+}
