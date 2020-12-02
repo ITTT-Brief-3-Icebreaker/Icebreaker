410ffb25c86424adcc3a61125e82ed4d05d387ab
Fanny Buhlér <fanny.buhler1@gmail.com>
2020-12-02 14:44:12 +0100

    ???

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,12 @@
 function getJokes() {
     // if (JSON.parse(localStorage.getItem('Jokes')) == undefined) {
     console.log('get new Jokes')
-    fetch('https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist&type=single&amount=100')
+    fetch('https://run.mocky.io/v3/dde3b884-8754-42e3-8356-dd95babeb521')
         .then(response => response.json())
         .then(data => {
-            localStorage.setItem('Jokes', JSON.stringify(data.jokes)),
+            localStorage.setItem('Jokes', JSON.stringify(data)),
                 setup()
         })
-
-    // localStorage.setItem('Clicked', JSON.stringify('Jokes'))
 }
 
 function getFacts() {
@@ -19,8 +17,6 @@ function getFacts() {
             localStorage.setItem('Facts', JSON.stringify(data.results)),
                 setup()
         })
-
-    // localStorage.setItem('Clicked', JSON.stringify('Facts'));
 }
 
 
@@ -48,6 +44,7 @@ function getConversation() {
         })
 }
 
+// if (window.location)
 var swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
